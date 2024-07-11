c0db60eb66efdba7887b217fc5b5ac9c26fcc417
MatthewCCChang <deadwalker1228@gmail.com>
2024-07-11 11:32:37 -0700

    Update redeplopyment script

--- redeploy.sh
+++ redeploy.sh
@@ -6,8 +6,8 @@ cd MLH_Portfolio
 git fetch
 git reset origin/main --hard
 
-tmux new -d -s portfolio
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
 
-tmux send-keys 'source python3-virtualenv/bin/activate' C-m
-tmux send-keys 'pip install -r requirements.txt' C-m
-tmux send-keys 'flask run --host=0.0.0.0' C-m
+systemctl daemon-reload
+systemctl portfolio
