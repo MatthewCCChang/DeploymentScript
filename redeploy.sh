472ac13ac88096544f7601ba37ac6c86b194c694
MatthewCCChang <deadwalker1228@gmail.com>
2024-07-25 11:36:42 -0700

    Update with docker

--- redeploy.sh
+++ redeploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-tmux kill-session
 cd MLH_Portfolio
 
 git fetch
 git reset origin/main --hard
 
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-
-systemctl daemon-reload
-systemctl enable mysqld
-systemctl portfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
